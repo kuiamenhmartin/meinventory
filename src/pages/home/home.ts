5332740c89917d72ede5157c350e89b9b60f41b1
Kuiamenh Martin <qsarza@wylog.com>
2018-06-14 17:53:53 +0800

    This is my intial push

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,Events } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  items = [
+    {'categroy': 1, 'name' : 'Vinegar', 'price' : 12.50, 'quantity': 5},
+    {'categroy': 1, 'name' : 'Ginebra', 'price' : 12.50, 'quantity': 5},
+    {'categroy': 1, 'name' : 'Palmolive', 'price' : 12.50, 'quantity': 5},
+    {'categroy': 1, 'name' : 'Creamsilk', 'price' : 12.50, 'quantity': 5},
+    {'categroy': 1, 'name' : 'Sunsilk', 'price' : 12.50, 'quantity': 5},
+    {'categroy': 1, 'name' : 'Kape Blanca', 'price' : 12.50, 'quantity': 5}
+  ];
 
+  // db = new PouchDB('todos');
+  // remoteCouch = false;
+
+  constructor(public navCtrl: NavController, public events: Events) {
+
+    // var myDB = window.sqlitePlugin.openDatabase({name: "mySQLite.db", location: 'default'});
   }
+  getItems(event: Event) {
+    console.log(event.target);
+  }
+
 
 }
